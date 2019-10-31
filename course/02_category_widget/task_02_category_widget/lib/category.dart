e8f1f140c998168097c2c4aa6422536138b51649
Joel Abraham Espinal Carrasco <j.espinalcarrasco@gmail.com>
2019-10-31 11:55:04 -0400

    Solve  todo by adding required properties and create category witget
    the category widget contains tow key children
    - icon
    - text

--- course/02_category_widget/task_02_category_widget/lib/category.dart
+++ course/02_category_widget/task_02_category_widget/lib/category.dart
@@ -16,7 +16,12 @@ class Category extends StatelessWidget {
   /// A [Category] saves the name of the Category (e.g. 'Length'), its color for
   /// the UI, and the icon that represents it (e.g. a ruler).
   // TODO: You'll need the name, color, and iconLocation from main.dart
-  const Category();
+  String _categoryName;
+  IconData _icon;
+  Color _iconColor;
+
+//  const Category();
+  Category(this._categoryName, this._icon, this._iconColor);
 
   /// Builds a custom widget that shows [Category] information.
   ///
@@ -28,6 +33,30 @@ class Category extends StatelessWidget {
   // See https://docs.flutter.io/flutter/material/Theme-class.html
   Widget build(BuildContext context) {
     // TODO: Build the custom widget here, referring to the Specs.
-    return Container();
+
+    return Container(
+        height: 100.0,
+        padding: EdgeInsets.all(8.0),
+        child: InkWell(
+          onTap: () => print('I was tapped!'),
+          child: Row(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Icon(
+                  _icon,
+                  color: _iconColor,
+                  size: 60.0,
+                ),
+              ),
+              Center(
+                child: Text(
+                  _categoryName,
+                  style: TextStyle(fontSize: 24.0),
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
